e64c876a573e89afafd97c5e65ea467d947b4077
behruz171 <behruzzo662@gmail.com>
2024-04-28 16:04:46 +0500

    nimadir

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+from .models import *
+
+
+admin.site.register(Banner)
+admin.site.register(Appeal)
+admin.site.register(Comment)
+admin.site.register(Meeting)
